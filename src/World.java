91a1fa55579ee27d2da1d42d11de00aa77b68514
Wha Chanunya <whachanunya@gmail.com>
2022-05-13 15:28:30 +0700

    set different objects

--- src/World.java
+++ src/World.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Observable;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class World extends Observable {
@@ -20,30 +17,22 @@ public class World extends Observable {
     private List<BlockTree> treeBlocks = new ArrayList<BlockTree>();
     private List<BlockBrick> brickBlocks = new ArrayList<BlockBrick>();
     private List<BlockSteel> steelBlocks = new ArrayList<BlockSteel>();
-    private String string;
+    private List<BlockStream> streamBlocks = new ArrayList<BlockStream>();
 
     public World(int size) {
         this.size = size;
         tick = 0;
-        player = new Player(size/2, size/2);
-        //  enemies = new Enemy[enemyCount];
-        //  enemiesStart = new Enemy[enemyCount];
-        Random random = new Random();
-        for(int i = 0; i < enemyCount; i++) {
-            int x = random.nextInt(size);
-            int y = random.nextInt(size);
-            enemies.add(new Enemy(x, y));
-            enemiesStart.add(new Enemy(x, y));
-        }
-        // enemies[enemies.length] = new Enemy((size/2), (size/2)+2);
+        player = new Player(13, 24);
+        setEnemies();
         setTreeBlocks();
         setSteelBlocks();
         setBrickBlocks();
+        setStreamBlocks();
     }
 
     public void start() {
         player.reset();
-        player.setPosition(size/2, size/2);
+        player.setPosition(13, 24);
         for(int i = 0; i < enemies.size(); i++) {
             enemies.get(i).setPosition(enemiesStart.get(i).getX(), enemiesStart.get(i).getY());
         }
@@ -54,7 +43,7 @@ public class World extends Observable {
             public void run() {
                 while(notOver) {
                     tick++;
-                    player.move();
+                    player.move(where(player));
                     for(int b = 0; b < player.getBullets().size(); b++) {
                         player.getBullets().get(b).move();
                     }
@@ -68,8 +57,7 @@ public class World extends Observable {
                     }
 
                     for(Enemy enemy: enemies) {
-                        string = where(enemy);
-                        enemy.moveTankEnermy(player.getX(), player.getY(), tick, string);
+                        enemy.moveTankEnermy(player.getX(), player.getY(), tick, where(enemy));
                         for (Bullet bullet : enemy.getBullets()){
                             bullet.move();
                         }
@@ -110,6 +98,7 @@ public class World extends Observable {
                     if (brickBlocks.get(brick).isBulletHit(enemies.get(i).getBullets().get(b))) {
                         enemies.get(i).getBullets().remove(enemies.get(i).getBullets().get(b));
                         brickBlocks.remove(brickBlocks.get(brick));
+                        break;
                     }
                 }
             }
@@ -119,6 +108,7 @@ public class World extends Observable {
                 if (brickBlocks.get(brick).isBulletHit(player.getBullets().get(b))) {
                     player.getBullets().remove(player.getBullets().get(b));
                     brickBlocks.remove(brickBlocks.get(brick));
+                    break;
                 }
             }
         }
@@ -145,32 +135,46 @@ public class World extends Observable {
         }
     }
 
-    private String where(WObject enemy) {
+    private String where(WObject obj) {
         for (BlockBrick brick : brickBlocks) {
-            if (brick.getX() == enemy.getX() && brick.getY() + 1 == enemy.getY()) {
+            if (brick.getX() == obj.getX() && brick.getY() + 1 == obj.getY()) {
                 return "up";
             }
-            if (brick.getX() == enemy.getX() && brick.getY() - 1 == enemy.getY()) {
+            if (brick.getX() == obj.getX() && brick.getY() - 1 == obj.getY()) {
                 return "down";
             }
-            if (brick.getX() + 1 == enemy.getX() && brick.getY() == enemy.getY()) {
+            if (brick.getX() + 1 == obj.getX() && brick.getY() == obj.getY()) {
                 return "left";
             }
-            if (brick.getX() - 1 == enemy.getX() && brick.getY() == enemy.getY()) {
+            if (brick.getX() - 1 == obj.getX() && brick.getY() == obj.getY()) {
                 return "right";
             }
         }
         for (BlockSteel steel : steelBlocks) {
-            if (steel.getX() == enemy.getX() && steel.getY() + 1 == enemy.getY()) {
+            if (steel.getX() == obj.getX() && steel.getY() + 1 == obj.getY()) {
                 return "up";
             }
-            if (steel.getX() == enemy.getX() && steel.getY() - 1 == enemy.getY()) {
+            if (steel.getX() == obj.getX() && steel.getY() - 1 == obj.getY()) {
                 return "down";
             }
-            if (steel.getX() + 1 == enemy.getX() && steel.getY() == enemy.getY()) {
+            if (steel.getX() + 1 == obj.getX() && steel.getY() == obj.getY()) {
                 return "left";
             }
-            if (steel.getX() - 1 == enemy.getX() && steel.getY() == enemy.getY()) {
+            if (steel.getX() - 1 == obj.getX() && steel.getY() == obj.getY()) {
+                return "right";
+            }
+        }
+        for (BlockStream stream : streamBlocks) {
+            if (stream.getX() == obj.getX() && stream.getY() + 1 == obj.getY()) {
+                return "up";
+            }
+            if (stream.getX() == obj.getX() && stream.getY() - 1 == obj.getY()) {
+                return "down";
+            }
+            if (stream.getX() + 1 == obj.getX() && stream.getY() == obj.getY()) {
+                return "left";
+            }
+            if (stream.getX() - 1 == obj.getX() && stream.getY() == obj.getY()) {
                 return "right";
             }
         }
@@ -188,6 +192,11 @@ public class World extends Observable {
                 return false;
             }
         }
+        for(BlockStream stream: streamBlocks){
+            if(stream.getX()==p.getX()&&stream.getY()+1==p.getY()){
+                return false;
+            }
+        }
         return true;
     }
 
@@ -199,6 +208,11 @@ public class World extends Observable {
         }
         for(BlockSteel steel: steelBlocks){
             if(steel.getX()==p.getX()&&steel.getY()-1==p.getY()){
+                return false;
+            }
+        }
+        for(BlockStream stream: streamBlocks){
+            if(stream.getX()==p.getX()&&stream.getY()-1==p.getY()){
                 return false;
             }
         }
@@ -216,6 +230,11 @@ public class World extends Observable {
                 return false;
             }
         }
+        for(BlockStream stream: streamBlocks){
+            if(stream.getX()+1==p.getX()&&stream.getY()==p.getY()){
+                return false;
+            }
+        }
         return true;
     }
 
@@ -227,6 +246,11 @@ public class World extends Observable {
         }
         for(BlockSteel steel: steelBlocks){
             if(steel.getX()-1==p.getX()&&steel.getY()==p.getY()){
+                return false;
+            }
+        }
+        for(BlockStream stream: streamBlocks){
+            if(stream.getX()-1==p.getX()&&stream.getY()==p.getY()){
                 return false;
             }
         }
@@ -261,6 +285,25 @@ public class World extends Observable {
         player.turnEast();
     }
 
+    private void setEnemies(){
+        for(int i = 0; i < enemyCount/2; i++) {
+            enemies.add(new Enemy(i*5+3, 0));
+            enemiesStart.add(new Enemy(i*5+3, 0));
+        }
+        enemies.add(new Enemy(3, 24));
+        enemiesStart.add(new Enemy(3, 24));
+        enemies.add(new Enemy(0, 18));
+        enemiesStart.add(new Enemy(0, 18));
+        enemies.add(new Enemy(0, 22));
+        enemiesStart.add(new Enemy(0, 22));
+        enemies.add(new Enemy(22, 24));
+        enemiesStart.add(new Enemy(22, 24));
+        enemies.add(new Enemy(24, 18));
+        enemiesStart.add(new Enemy(24, 18));
+        enemies.add(new Enemy(24, 22));
+        enemiesStart.add(new Enemy(24, 22));
+    }
+
     public List<Enemy> getEnemies() {
         return enemies;
     }
@@ -270,18 +313,13 @@ public class World extends Observable {
     }
 
     private void setTreeBlocks(){
-        BlockTree tree = new BlockTree(10, 10);
-        BlockTree tree2 = new BlockTree(11, 10);
-        BlockTree tree3 = new BlockTree(12, 10);
-        BlockTree tree4 = new BlockTree(10, 11);
-        BlockTree tree5 = new BlockTree(11, 11);
-        BlockTree tree6 = new BlockTree(12, 11);
-        treeBlocks.add(tree);
-        treeBlocks.add(tree2);
-        treeBlocks.add(tree3);
-        treeBlocks.add(tree4);
-        treeBlocks.add(tree5);
-        treeBlocks.add(tree6);
+        for(int j=0;j<25;j++){
+            if((3<=j&&j<=6)||(10<=j&&j<=12)||(18<=j&&j<=21)){
+                continue;
+            }
+            treeBlocks.add(new BlockTree(j, 16));
+            treeBlocks.add(new BlockTree(j, 17));
+        }
     }
 
     public List<BlockTree> getTreeBlocks() {
@@ -289,12 +327,20 @@ public class World extends Observable {
     }
 
     private void setBrickBlocks(){
-        BlockBrick brick = new BlockBrick(8, 4);
-        BlockBrick brick2 = new BlockBrick(9, 4);
-        BlockBrick brick3 = new BlockBrick(10, 4);
-        brickBlocks.add(brick);
-        brickBlocks.add(brick2);
-        brickBlocks.add(brick3);
+        for(int j=17; j<25; j++) {
+            brickBlocks.add(new BlockBrick(j, 5));
+            brickBlocks.add(new BlockBrick(j, 7));
+        }
+        for(int j=6;j<17;j++){
+            brickBlocks.add(new BlockBrick(j, 13));
+            brickBlocks.add(new BlockBrick(j, 14));
+        }
+        for(int j=18;j<25;j++){
+            brickBlocks.add(new BlockBrick(6, j));
+            brickBlocks.add(new BlockBrick(7, j));
+            brickBlocks.add(new BlockBrick(15, j));
+            brickBlocks.add(new BlockBrick(16, j));
+        }
     }
 
     public List<BlockBrick> getBrickBlocks() {
@@ -302,12 +348,26 @@ public class World extends Observable {
     }
 
     private void setSteelBlocks(){
-        BlockSteel steel = new BlockSteel(16, 18);
-        BlockSteel steel2 = new BlockSteel(17, 18);
-        BlockSteel steel3 = new BlockSteel(18, 18);
-        steelBlocks.add(steel);
-        steelBlocks.add(steel2);
-        steelBlocks.add(steel3);
+        for(int j=5; j<14; j++) {
+            steelBlocks.add(new BlockSteel(j, 5));
+            steelBlocks.add(new BlockSteel(j, 6));
+            steelBlocks.add(new BlockSteel(j, 7));
+        }
+        for(int j=18; j<25; j++) {
+            steelBlocks.add(new BlockSteel(19, j));
+            steelBlocks.add(new BlockSteel(20, j));
+        }
+    }
+
+    private void setStreamBlocks(){
+        for(int j=2; j<12; j++) {
+            streamBlocks.add(new BlockStream(3, j));
+            streamBlocks.add(new BlockStream(4, j));
+        }
+    }
+
+    public List<BlockStream> getStreamBlocks() {
+        return streamBlocks;
     }
 
     public List<BlockSteel> getSteelBlocks() {
